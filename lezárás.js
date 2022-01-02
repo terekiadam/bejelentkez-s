f4f6cd5848c48f60ee5627d73a4fa45c3b4735ad
terekiadam <89510387+terekiadam@users.noreply.github.com>
2022-01-02 15:22:37 +0100

    Add files via upload

---
+++ lezárás.js
@@ -0,0 +1,12 @@
+function c() {
+
+let a = parseInt (
+    document.getElementById('a').value );
+    let b = parseInt (
+        document.getElementById('b').value );
+
+if (a === 1 ){
+    if (b === 8712){open("1.html")}
+    location.reload();
+}
+}
